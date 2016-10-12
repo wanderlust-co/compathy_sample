02b7a19c5f3e15d68b03640e4d9823de3a9f28bd
minamimasanobu <west013minami@gmail.com>
2016-10-12 10:55:01 +0900

    fix aws keys and bucket_name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
     s3_host_name: "s3.amazonaws.com",
     s3_protocol: :https,
     s3_credentials: {
-      bucket:            'compathy-clone',
+      bucket:            ENV['S3_BUCKET_NAME'],
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
